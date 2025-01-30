56246a6f32b32698c968953a3f91763a1d8b233b
khd325 <hdk325@naver.com>
2025-01-30 16:29:25 +0900

    스프링 AOP 구현

--- aop/src/main/java/hello/aop/AopApplication.java
+++ aop/src/main/java/hello/aop/AopApplication.java
@@ -1,9 +1,17 @@
 package hello.aop;
 
+import hello.aop.order.aop.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
